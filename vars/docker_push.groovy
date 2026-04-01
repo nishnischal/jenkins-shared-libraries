c9881357f7579bcf1db0a6095f86deda9cc77054
nishnischal <133730785+nishnischal@users.noreply.github.com>
2026-04-01 17:43:26 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag, String dockerhubuser) {
     withCredentials([usernamePassword(
-        credentialsId: 'docker',
+        credentialsId: 'docker-hub-cred',
         passwordVariable: 'dockerHubPass',
         usernameVariable: 'dockeHubUser'
     )]) {
